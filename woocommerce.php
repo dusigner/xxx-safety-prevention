188879a09021edff4e02efa5e80fa1fd5f522397
Eduardo Soares <e.soares@samsung.com>
2026-05-06 15:23:14 -0300

    added archive-product.php

--- woocommerce.php
+++ woocommerce.php
@@ -5,6 +5,13 @@
  * @package XXX_Safety_Prevention
  */
 
+defined( 'ABSPATH' ) || exit;
+
+if ( function_exists( 'is_shop' ) && ( is_shop() || ( function_exists( 'is_product_taxonomy' ) && is_product_taxonomy() ) ) ) {
+	wc_get_template( 'archive-product.php' );
+	return;
+}
+
 get_header();
 ?>
 <main id="primary" class="site-main site-main-shop">
